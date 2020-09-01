05cf804bbe41f33c974fcf3d27a291a8d9256dfd
Mahmood Abushaireh <maabusha@microsoft.com>
2020-09-01 10:35:49 +0300

    Install Posh-Ssh Lib

--- src/HDICloner/utils/SshUtils.ps1
+++ src/HDICloner/utils/SshUtils.ps1
@@ -1,1 +1,15 @@
 #Intilliaze
+
+function Setup()
+{
+    if (Get-Module -ListAvailable -Name "Posh-SSH") {
+        Show-Info "Posh-Ssh module is installed"
+    } 
+    else {
+        Show-Info "Posh-Ssh module doesnt exist, Installing it"
+        
+        Install-Module -Name "Posh-SSH"
+    }
+
+    Import-Module -Name "Posh-SSH"
+}
